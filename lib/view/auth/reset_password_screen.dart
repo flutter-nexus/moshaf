af5dbc8859dabb2b01c30694c2dfd2154ca9f78a
zemax.c4 <zemax.c4@gmail.com>
2024-08-22 10:44:31 +0300

    improved  forget password

--- lib/view/auth/reset_password_screen.dart
+++ lib/view/auth/reset_password_screen.dart
@@ -1,48 +1,23 @@
 import 'package:flutter/material.dart';
 
-class ResetPasswordScreen extends StatefulWidget {
-  @override
-  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
-}
+class ResetPasswordScreen extends StatelessWidget {
+  final ResetPasswordController _controller = ResetPasswordController();
 
-class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  bool _isLoading = false;
-
-  void _resetPassword() {
-    if (_formKey.currentState!.validate()) {
-      setState(() {
-        _isLoading = true;
-      });
-
-      // Simulate a network request to send reset password email
-      Future.delayed(Duration(seconds: 2), () {
-        setState(() {
-          _isLoading = false;
-        });
-        // Display a success message or navigate to another screen
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Password reset email sent!'),
-          backgroundColor: Colors.green,
-        ));
-      });
-    }
-  }
+  ResetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 50),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 50),
           child: Form(
-            key: _formKey,
+            key: _controller.formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                SizedBox(height: 15),
-                Text(
+                const SizedBox(height: 15),
+                const Text(
                   "Reset Password",
                   style: TextStyle(
                     fontSize: 28,
@@ -51,71 +26,115 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
                     fontFamily: 'NotoNaskhArabic',
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Image.asset(
-                  'assets/images/forgetpassword.png', // Use an image that reflects password reset
+                  'assets/images/forgetpassword.png',
                   height: MediaQuery.sizeOf(context).height * 0.3,
                 ),
-                SizedBox(height: 20),
-                Text(
+                const SizedBox(height: 20),
+                const Text(
                   "Enter your email and we'll send you a code to reset your password.",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 18, color: Colors.black54),
                 ),
-                SizedBox(height: 40),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                    prefixIcon: Icon(Icons.email, color: Colors.teal),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 40),
-                _isLoading
-                    ? CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: _resetPassword,
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: Text(
-                          'Reset Password',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.teal,
-                          ),
-                        ),
-                      ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Go back to the previous screen
-                  },
-                  child: Text(
-                    'Back to Login',
-                    style: TextStyle(color: Colors.teal),
-                  ),
-                ),
+                const SizedBox(height: 40),
+                _buildEmailField(_controller),
+                const SizedBox(height: 40),
+                _controller.isLoading
+                    ? const CircularProgressIndicator()
+                    : _buildResetPasswordButton(context, _controller),
+                const SizedBox(height: 20),
+                _buildBackToLoginButton(context),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildEmailField(ResetPasswordController controller) {
+    return TextFormField(
+      controller: controller.emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'Enter your email',
+        prefixIcon: const Icon(Icons.email, color: Colors.teal),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+          return 'Enter a valid email';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildResetPasswordButton(
+      BuildContext context, ResetPasswordController controller) {
+    return ElevatedButton(
+      onPressed: () => controller.resetPassword(context),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.sizeOf(context).width * 0.2,
+          vertical: MediaQuery.sizeOf(context).height * 0.02,
+        ),
+      ),
+      child: const Text(
+        'Reset Password',
+        style: TextStyle(
+          fontSize: 18,
+          color: Colors.teal,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackToLoginButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pop(context); // Go back to the previous screen
+      },
+      child: const Text(
+        'Back to Login',
+        style: TextStyle(color: Colors.teal),
+      ),
+    );
+  }
+}
+
+class ResetPasswordController with ChangeNotifier {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  bool isLoading = false;
+
+  void resetPassword(BuildContext context) async {
+    if (formKey.currentState!.validate()) {
+      isLoading = true;
+      notifyListeners();
+
+      // Simulate a network request to send reset password email
+      await Future.delayed(const Duration(seconds: 2));
+
+      isLoading = false;
+      notifyListeners();
+
+      // Display a success message or navigate to another screen
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Password reset email sent!'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    }
   }
 }
