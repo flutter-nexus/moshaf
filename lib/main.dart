dbb5d1e2bcd589ecafaa4f968d57c07007db698d
Mohamed Emad <zemax.c4@gmail.com>
2024-08-25 17:46:03 +0300

    add home dev

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:moshaf/view/quran/time_of_prayer.dart';
 import 'package:moshaf/view/settings/settings.dart';
 
 void main() {
-
   runApp(const MyApp());
 }
 
@@ -18,10 +17,83 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: QuranTimeScreen(),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List<String> titles = [
+    'Home Screen',
+    'Login Screen',
+    'Signup Screen',
+    'Reset Password Screen',
+    'Verification Code Screen',
+    'Settings Screen',
+    'Time of Prayer Screen',
+    'Quran Index Screen',
+  ];
+  final List<Widget> pages = [
+    HomeScreen(),
+    LoginScreen(),
+    SignUpScreen(),
+    ResetPasswordScreen(),
+    VerificationCodeInput(
+      controller: TextEditingController(),
+      onCompleted: (value) {
+        print(value);
+      },
+    ),
+    SettingsScreen(),
+    QuranTimeScreen(),
+    QuranIndexPage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titles List'),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: titles.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Center(child: Text(titles[index])),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return pages[index];
+                  }),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final String title;
+
+  DetailPage({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text('Welcome to $title'),
       ),
     );
   }
