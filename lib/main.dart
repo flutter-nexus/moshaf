7e3de4a814753941e4a88c9f9ec74f11b2b4148e
Mohamed Emad <zemax.c4@gmail.com>
2024-08-25 18:44:39 +0300

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('page List'),
+        title: Text('page  List'),
       ),
       body: Center(
         child: ListView.builder(
