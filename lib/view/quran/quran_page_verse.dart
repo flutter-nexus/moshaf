8a8ba147eb6280f94a7ccf9d0b3d8440872b6a84
Mohamed Emad <zemax.c4@gmail.com>
2024-08-28 08:06:11 +0300

    .

--- lib/view/quran/quran_page_verse.dart
+++ lib/view/quran/quran_page_verse.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class QuranPageVersePreview extends StatefulWidget {
-  QuranPageVersePreview({Key? key, required this.indexSurah, required this.surahName}) : super(key: key);
+  QuranPageVersePreview(
+      {Key? key, required this.indexSurah, required this.surahName})
+      : super(key: key);
   final int indexSurah;
-  final String surahName ;
+  final String surahName;
 
   @override
   _QuranPageVersePreviewState createState() => _QuranPageVersePreviewState();
@@ -14,11 +18,31 @@ class QuranPageVersePreview extends StatefulWidget {
 class _QuranPageVersePreviewState extends State<QuranPageVersePreview> {
   List<dynamic> ayahs = [];
   bool isLoading = true;
+   Map<String, dynamic> exegesis = {};
 
   @override
   void initState() {
     super.initState();
     loadQuranData();
+    fetchExegesis();
+  }
+
+  Future<Map<String, dynamic>> getExegesisSurah() async {
+    final dio = Dio();
+    final response = await dio.get(
+      'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/ara-jalaladdinalmah.min.json',
+    );
+     exegesis =await response.data;
+    return response.data;
+  }
+
+  Future<void> fetchExegesis() async {
+    try {
+      exegesis = await getExegesisSurah();
+      setState(() {}); // Refresh UI after fetching data
+    } catch (e) {
+      print("Error fetching exegesis data: $e");
+    }
   }
 
   Future<void> loadQuranData() async {
@@ -53,38 +77,53 @@ class _QuranPageVersePreviewState extends State<QuranPageVersePreview> {
                   itemBuilder: (context, index) {
                     return Column(
                       children: [
-                        ListTile(
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Stack(
-                                alignment: Alignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.circle,
-                                    size: 30,
-                                    color: Colors.grey,
-                                  ),
-                                  Positioned(
-                                    child: Text(
-                                      '${ayahs[index]['verse']}', // Verse number
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.black,
+                        GestureDetector(
+                          onTap: () async {
+                            try {
+                             
+                              if (exegesis != null) {
+                                var quranText = exegesis['quran'][ayahs[index]]['text'];
+                                log(quranText);
+                              } else {
+                                log('Exegesis data not available');
+                              }
+                            } catch (e) {
+                              log('Error accessing exegesis data: $e');
+                            }
+                          },
+                          child: ListTile(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.circle,
+                                      size: 30,
+                                      color: Colors.grey,
+                                    ),
+                                    Positioned(
+                                      child: Text(
+                                        '${ayahs[index]['verse']}', // Verse number
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.black,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  ayahs[index]['text'],
-                                  textAlign: TextAlign.right,
-                                  style: TextStyle(fontSize: 24),
+                                  ],
                                 ),
-                              ),
-                            ],
+                                SizedBox(width: 10),
+                                Expanded(
+                                  child: Text(
+                                    ayahs[index]['text'],
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                         Divider(
