2dc1cc6e63e85988b24700d6ad9d4ca6bdd4c1a9
Mohamed Emad <zemax.c4@gmail.com>
2024-08-30 18:39:42 +0300

    ..

--- lib/view/quran/whole_surah_quran.dart
+++ lib/view/quran/whole_surah_quran.dart
@@ -1,19 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:moshaf/imports/imports.dart';
 import 'package:quran/quran.dart' as quran;
 
-class WholeQuranSurah extends StatelessWidget {
-   WholeQuranSurah({super.key});
- String text = '${quran.getVerse(18, 1, verseEndSymbol: true)}';
-  @override 
+class WholeQuranSurah extends StatefulWidget {
+  WholeQuranSurah({super.key});
+
+  @override
+  State<WholeQuranSurah> createState() => _WholeQuranSurahState();
+}
+
+class _WholeQuranSurahState extends State<WholeQuranSurah> {
+  List<String> verses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchQuranPageText();
+  }
+
+  Future<void> fetchQuranPageText() async {
+    List<String> fetchedVerses = [];
+    for (int i = quran.getVerseCountByPage(2) + 1; i <= quran.getVerseCountByPage(3); i++) {
+      fetchedVerses.add(await quran.getVerse(2, i, verseEndSymbol: true));
+    }
+
+    setState(() {
+      verses = fetchedVerses;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('القرآن الكريم'),
         centerTitle: true,
       ),
-      body:  Center(
-        child: Text(text),
-      ),
+      body: verses.isNotEmpty
+          ? Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Center(
+                child: Wrap(
+                  alignment: WrapAlignment.end,
+                  textDirection: TextDirection.rtl,
+                  children: verses.map((verse) {
+                    return Text(
+                      verse + ' ',
+                      style: GoogleFonts.amiri(fontSize: 15),
+                      textAlign: TextAlign.justify,
+                    );
+                  }).toList(),
+                ),
+              ),
+            )
+          : const Center(child: CircularProgressIndicator()),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
