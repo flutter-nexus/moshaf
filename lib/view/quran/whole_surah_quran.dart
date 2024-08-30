41662ca83b452c35fccf34b7caed4d7ef308e954
Mohamed Emad <zemax.c4@gmail.com>
2024-08-30 22:18:02 +0300

    ..

--- lib/view/quran/whole_surah_quran.dart
+++ lib/view/quran/whole_surah_quran.dart
@@ -1,62 +1,32 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:moshaf/imports/imports.dart';
-import 'package:quran/quran.dart' as quran;
+import 'package:pdfrx/pdfrx.dart';
 
 class WholeQuranSurah extends StatefulWidget {
-  WholeQuranSurah({super.key});
+  const WholeQuranSurah({super.key});
 
   @override
   State<WholeQuranSurah> createState() => _WholeQuranSurahState();
 }
 
 class _WholeQuranSurahState extends State<WholeQuranSurah> {
-  List<String> verses = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchQuranPageText();
-  }
-
-  Future<void> fetchQuranPageText() async {
-    List<String> fetchedVerses = [];
-    for (int i = quran.getVerseCountByPage(2) + 1; i <= quran.getVerseCountByPage(3); i++) {
-      fetchedVerses.add(await quran.getVerse(2, i, verseEndSymbol: true));
-    }
-
-    setState(() {
-      verses = fetchedVerses;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('القرآن الكريم'),
-        centerTitle: true,
+        title: const Text('Pdfrx example'),
       ),
-      body: verses.isNotEmpty
-          ? Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Center(
-                child: Wrap(
-                  alignment: WrapAlignment.end,
-                  textDirection: TextDirection.rtl,
-                  children: verses.map((verse) {
-                    return Text(
-                      verse + ' ',
-                      style: GoogleFonts.amiri(fontSize: 15),
-                      textAlign: TextAlign.justify,
-                    );
-                  }).toList(),
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: SingleChildScrollView(
+              child: PdfViewer.asset(
+                initialPageNumber: 2,
+                params: PdfViewerParams(
+                  
                 ),
-              ),
-            )
-          : const Center(child: CircularProgressIndicator()),
-      bottomNavigationBar: CustomBottomNavigationBar(),
+            'assets/quran.pdf',
+            controller: PdfViewerController(),
+          ))),
     );
   }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,9 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   geolocator_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  pdfrx
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
