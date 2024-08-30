1228cc9b4146cdf6f2d710b8acd79c929357ec84
Mohamed Emad <zemax.c4@gmail.com>
2024-08-30 16:22:12 +0300

    ..

--- lib/view/quran/whole_surah_quran.dart
+++ lib/view/quran/whole_surah_quran.dart
@@ -1,17 +1,18 @@
 import 'package:moshaf/imports/imports.dart';
+import 'package:quran/quran.dart' as quran;
 
 class WholeQuranSurah extends StatelessWidget {
-  const WholeQuranSurah({super.key});
-
-  @override
+   WholeQuranSurah({super.key});
+ String text = '${quran.getVerse(18, 1, verseEndSymbol: true)}';
+  @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('القرآن الكريم'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('القرآن الكريم'),
+      body:  Center(
+        child: Text(text),
       ),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
