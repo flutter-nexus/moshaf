b8d791c9b23f65df8e49f05acc6d58ca5a4db8ae
mo <zemax.c4@gmail.com>
2025-01-06 22:23:56 +0200

    add quran page

---
+++ lib/view/quran/quran_page.dart
@@ -0,0 +1,337 @@
+import 'dart:async';
+
+import 'package:easy_container/easy_container.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart' as m;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:moshaf/view/quran/widgets/basmallah.dart';
+import 'package:quran/quran.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
+// import 'package:quran_tutorial/globalhelpers/constants.dart';
+// import 'package:quran_tutorial/widgets/basmallah.dart';
+// import 'package:quran_tutorial/widgets/header_widget.dart';
+
+import 'widgets/header_widget.dart';
+
+class QuranViewPage extends StatefulWidget {
+  int pageNumber;
+  var jsonData;
+  var shouldHighlightText;
+  var highlightVerse;
+  QuranViewPage({
+    Key? key,
+    required this.pageNumber,
+    required this.jsonData,
+    required this.shouldHighlightText,
+    required this.highlightVerse,
+  }) : super(key: key);
+
+  @override
+  State<QuranViewPage> createState() => _QuranViewPageState();
+}
+
+class _QuranViewPageState extends State<QuranViewPage> {
+  var highlightVerse;
+  var shouldHighlightText;
+  List<GlobalKey> richTextKeys = List.generate(
+    604, // Replace with the number of pages in your PageView
+    (_) => GlobalKey(),
+  );
+  setIndex() {
+    setState(() {
+      index = widget.pageNumber;
+    });
+  }
+
+  int index = 0;
+  late PageController _pageController;
+  late Timer timer;
+  String selectedSpan = "";
+
+  highlightVerseFunction() {
+    setState(() {
+      shouldHighlightText = widget.shouldHighlightText;
+    });
+    if (widget.shouldHighlightText) {
+      setState(() {
+        highlightVerse = widget.highlightVerse;
+      });
+
+      Timer.periodic(const Duration(milliseconds: 400), (timer) {
+        if (mounted) {
+          setState(() {
+            shouldHighlightText = false;
+          });
+        }
+        Timer(const Duration(milliseconds: 200), () {
+          if (mounted) {
+            setState(() {
+              shouldHighlightText = true;
+            });
+          }
+          if (timer.tick == 4) {
+            if (mounted) {
+              setState(() {
+                highlightVerse = "";
+
+                shouldHighlightText = false;
+              });
+            }
+            timer.cancel();
+          }
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    setIndex();
+    _pageController = PageController(initialPage: index);
+    highlightVerseFunction();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    WakelockPlus.enable();
+// TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // timer.cancel();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+    WakelockPlus.disable();
+    
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+        body: PageView.builder(
+      reverse: true,
+      scrollDirection: Axis.horizontal,
+      onPageChanged: (a) {
+        setState(() {
+          selectedSpan = "";
+        });
+        index = a;
+        // print(index)  ;
+      },
+      controller: _pageController,
+      // onPageChanged: _onPageChanged,
+      itemCount: totalPagesCount + 1 /* specify the total number of pages */,
+      itemBuilder: (context, index) {
+        bool isEvenPage = index.isEven;
+
+        if (index == 0) {
+          return Container(
+            color: const Color(0xffFFFCE7),
+            child: Image.asset(
+              "assets/images/jpg",
+              fit: BoxFit.fill,
+            ),
+          );
+        }
+
+        return Container(
+          decoration: const BoxDecoration(
+            color: Color(0xffF1EEE5),
+          ),
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.transparent,
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 12.0, left: 12),
+                child: SingleChildScrollView(
+                  // physics: const ClampingScrollPhysics(),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: screenSize.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: (screenSize.width * .27),
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      icon: const Icon(
+                                        Icons.arrow_back_ios,
+                                        size: 24,
+                                      )),
+                                  Text(
+                                      widget.jsonData[getPageData(index)[0]
+                                              ["surah"] -
+                                          1]["name"],
+                                      style: const TextStyle(
+                                          fontFamily: "Taha", fontSize: 14)),
+                                ],
+                              ),
+                            ),
+                            EasyContainer(
+                              borderRadius: 12,
+                              color: Colors.orange.withOpacity(.5),
+                              showBorder: true,
+                              height: 20,
+                              width: 120,
+                              padding: 0,
+                              margin: 0,
+                              child: Center(
+                                child: Text(
+                                  "${"page"} $index ",
+                                  style: const TextStyle(
+                                    fontFamily: 'aldahabi',
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: (screenSize.width * .27),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.settings,
+                                        size: 24,
+                                      ))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      if ((index == 1 || index == 2))
+                        SizedBox(
+                          height: (screenSize.height * .15),
+                        ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Directionality(
+                          textDirection: m.TextDirection.rtl,
+                          child: Padding(
+                            padding: const EdgeInsets.all(0.0),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: RichText(
+                                key: richTextKeys[index - 1],
+                                textDirection: m.TextDirection.rtl,
+                                textAlign:
+                                    (index == 1 || index == 2 || index > 570)
+                                        ? TextAlign.center
+                                        : TextAlign.center,
+                                softWrap: true,
+                                locale: const Locale("ar"),
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    color: m.Colors.black,
+                                    fontSize: 23.sp.toDouble(),
+                                  ),
+                                  children: getPageData(index).expand((e) {
+                                    List<InlineSpan> spans = [];
+                                    for (var i = e["start"];
+                                        i <= e["end"];
+                                        i++) {
+                                      // Header
+                                      if (i == 1) {
+                                        spans.add(WidgetSpan(
+                                          child: HeaderWidget(
+                                              e: e, jsonData: widget.jsonData),
+                                        ));
+                                        if (index != 187 && index != 1) {
+                                          spans.add(WidgetSpan(
+                                            
+                                            child: Basmallah(index: 0),
+                                          ));
+                                        }
+                                      if (index == 187) {
+                                          spans.add(WidgetSpan(
+                                            child: Container(
+                                              height: 10.h,
+                                            ),
+                                          ));
+                                        }
+                                      }
+
+                                      // Verses
+                                      spans.add(TextSpan(
+                                        recognizer: LongPressGestureRecognizer()
+                                          ..onLongPress = () {
+                                    
+                                            print("longpressed");
+                                          }
+                                          ..onLongPressDown = (details) {
+                                            setState(() {
+                                              selectedSpan = " ${e["surah"]}$i";
+                                            });
+                                          }
+                                          ..onLongPressUp = () {
+                                            setState(() {
+                                              selectedSpan = "";
+                                            });
+                                            print("finished long press");
+                                          }
+                                          ..onLongPressCancel =
+                                              () => setState(() {
+                                                    selectedSpan = "";
+                                                  }),
+                                        text: i == e["start"]
+                                            ? "${getVerseQCF(e["surah"], i).replaceAll(" ", "").substring(0, 1)}\u200A${getVerseQCF(e["surah"], i).replaceAll(" ", "").substring(1)}"
+                                            : getVerseQCF(e["surah"], i)
+                                                .replaceAll(' ', ''),
+                
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          height: (index == 1 || index == 2)
+                                              ? 2.h
+                                              : 1.95.h,
+                                          letterSpacing: 0.w,
+                                          wordSpacing: 0,
+                                          fontFamily:
+                                              "QCF_P${index.toString().padLeft(3, "0")}",
+                                          fontSize: index == 1 || index == 2
+                                              ? 28.sp
+                                              : index == 145 || index == 201
+                                                  ? index == 532 || index == 533
+                                                      ? 22.5.sp
+                                                      : 22.4.sp
+                                                  : 23.1.sp,
+                                          backgroundColor: Colors.transparent,
+                                        ),
+                                        children: const <TextSpan>[
+                                     
+                                        ],
+                                      ));
+                                    }
+                                    return spans;
+                                  }).toList(),
+                                ),
+                              ),
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ); /* Your page content */
+      },
+    ));
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,10 +11,12 @@ import flutter_local_notifications
 import flutter_timezone
 import geolocator_apple
 import just_audio
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
@@ -23,8 +25,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
